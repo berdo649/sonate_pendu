c8fbf30584bc31b744255efd4ed8ce1230538f75
berdo649 <berdo649@gmail.com>
2026-04-21 19:59:10 +0200

    Ajout page d'accueil avec formulaire et route /jeu

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template("accueil.html")
+
+
+@app.route("/jeu", methods=["POST"])
+def jeu():
+    nom = request.form["nom"]
+    return f"<h1>Bonjour {nom} ! La partie va bientôt commencer...</h1>"
